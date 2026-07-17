60173297ce82167b16b72eb4b06460a1ed46803c
Lino <sauvairelino@gmail.com>
2026-07-17 14:39:31 +0200

    update dependencies

--- christianramade.fr/next.config.ts
+++ christianramade.fr/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "christianramade.s3.eu-north-1.amazonaws.com",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.eu-north-1.amazonaws.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
